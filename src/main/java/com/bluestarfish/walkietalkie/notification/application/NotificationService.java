557e3462588f8fd851912db2cd81c948806bd5c5
BenchPress200 <jms0538@naver.com>
2024-10-25 02:49:36 +0900

    test

--- src/main/java/com/bluestarfish/walkietalkie/notification/application/NotificationService.java
+++ src/main/java/com/bluestarfish/walkietalkie/notification/application/NotificationService.java
@@ -38,8 +38,9 @@ public class NotificationService extends ListenerAdapter {
         }
     }
 
-    @Scheduled(cron = "0 5 2 * * *", zone = TIMEZONE)
+    @Scheduled(cron = "0 55 3 * * *", zone = TIMEZONE)
     public void sendStudyRecordNotification() {
+        log.info("채널 ID {}", channelId);
         log.info("공부시간 기록 알림 메시지 전송 실행");
         if (textChannel != null) {
             textChannel.sendMessage(STUDY_TIME_RECORD_MESSAGE + " 테슽흐").queue();
